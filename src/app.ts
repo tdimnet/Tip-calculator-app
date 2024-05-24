2be79a3838c7154a8618745b4978497c507140cf
tdimnet <thomas.dimnet@gmail.com>
2024-05-24 16:12:22 +0200

    Writing TS for handling event

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+const $formTip = document.querySelector('.form-tip')
+
+function onUpdateBill() {
+    const billValue = (document.querySelector('#bill') as HTMLInputElement).value
+
+    console.log(billValue)
+}
+
+
+$formTip?.addEventListener('input', function(e) {
+    const targetElement = e.target as HTMLInputElement
+    const dataType = targetElement.getAttribute('data-type')
+
+    if (dataType === "bill") {
+        onUpdateBill()
+    } else if (dataType === "tip") {
+        console.log("Let's update the tip")
+    } else if (dataType === "people") {
+        console.log("Let's update the people")
+    } else {
+        throw new Error("Unknown data type")
+    }
+})
+
 function main(): void {
     console.log("Hello, Thomas!")
 }
